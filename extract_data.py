e6dae6556f8f9f8370ba47a5cf35428df137f17e
Tashrif Billah <tashrifbillah@gmail.com>
2018-01-20 18:51:18 -0500

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -1,22 +1,24 @@
 import os
 from os import listdir
 from os.path import isfile, join
-import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 import xmltodict
-from scipy import io
 from scipy import misc
 import pickle
 
 
+# Defining constants
 ROOT_PATH = "VOCtrainval_11-May-2012/VOCdevkit/VOC2012"
 ANNOTATION_FOLDER = "Annotations"
 IMAGE_FOLDER = "JPEGImages"
 CLASSES = ["person", "bird", "cat", "cow", "dog", "horse", "sheep", "aeroplane", "bicycle", "boat", "bus", "car",
            "motorbike", "train", "bottle", "chair", "diningtable", "pottedplant", "sofa", "tvmonitor"]
+
 CLASS_TO_LABEL = {class_name: label for (class_name, label) in zip(CLASSES, range(len(CLASSES)))}
 
+
+# Defining functions for extracting relevant data from the VOC 2012 database
 def extract_objects(xml):
     objects = xml['annotation']['object']
     return [objects] if isinstance(objects,xmltodict.OrderedDict) else objects
@@ -30,6 +32,17 @@ def extract_bounding_boxes(xml):
                    for key in ['xmin', 'ymin', 'xmax', 'ymax']]) for object in extract_objects(xml)]
 
 
+
+def format_data(data):
+
+    data_tuples = [(value["image"],value["labels"],value["bounding_boxes"]) for key,value in data.items()]
+
+    values = [(image, bounding_boxes,labels)
+              for (image, subimages, labels, bounding_boxes) in data_tuples]
+
+    return values
+
+
 def process_data():
 
     annotation_directory_path = os.path.join(ROOT_PATH, ANNOTATION_FOLDER)
@@ -37,6 +50,7 @@ def process_data():
 
     names_and_image_paths_and_xml_paths = [
         (os.path.splitext(filename)[0],join(image_directory_path, os.path.splitext(filename)[0] + ".jpg"), join(annotation_directory_path, filename))
+
         for filename in listdir(annotation_directory_path)]
 
     names_and_image_paths_and_xml_paths = [(name,image_path, annotation_path)
@@ -45,37 +59,36 @@ def process_data():
 
 
     data = {}
+
     for (name,image_path, annotation_path) in names_and_image_paths_and_xml_paths:
 
         xml = xmltodict.parse(open(annotation_path, 'rb'))
-        image = misc.imread(image_path, mode='RGB')
+
         labels = extract_labels(xml)
-        bounding_boxes = extract_bounding_boxes(xml)
 
         data[name] = {
-            "image":image,
-            "bounding_boxes":bounding_boxes,
-            "labels": labels
+            "labels":labels
         }
 
         print("Processed %s" % name)
 
-    data_list = [(data[key]["image"],data[key]["bounding_boxes"][0],data[key]["labels"]) for (key,value) in data.items() if len(data[key]["labels"]) == 1]
-    images, bounding_boxes, labels = tuple([[data_list[i][j] for i in range(len(data_list))] for j in range(3)])
 
-    try:
-        os.mkdir("out2")
-    except:
-        pass
+    formatted_data = format_data(data)
 
-    for image,i in zip(images, range(len(images))):
-        sp.misc.imsave("out2/"+str(i)+".png",image)
+    rl_data = formatted_data
 
 
-    # np.savetxt("bb.csv",bounding_boxes,delimiter=',',newline='\n')
-    # np.savetxt("labels.csv",labels,delimiter=',',newline='\n')
+    for ((image,bounding_boxes,labels),i) in zip(rl_data,range(len(rl_data))):
+        sp.misc.imsave("out_rl/"+str(i)+".png",image)
+        print(str(i))
+
+
+    bounding_boxes = [bounding_boxes for (image,bounding_boxes,labels) in rl_data]
     pickle.dump(bounding_boxes, open("bounding_boxes.p", "wb"),protocol=4)
-    pickle.dump(labels, open("labels_rl.p", "wb"),protocol=4)
+
+
+    labels_rl = [labels for (image,bounding_boxes,labels) in rl_data]
+    pickle.dump(labels_rl, open("labels_rl.p", "wb"),protocol=4)
 
 
 process_data()
