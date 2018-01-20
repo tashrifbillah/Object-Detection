e80d1fae619504ca59fa186bd392beca5ac9eee7
Tashrif Billah <tashrifbillah@gmail.com>
2018-01-19 23:28:57 -0500

    Add files via upload

---
+++ SVM_SCORE_VGG19.py
@@ -0,0 +1,79 @@
+import numpy as np
+import os
+import scipy as sp
+from scipy import misc
+import matplotlib.pyplot as plt
+import time
+from numpy import genfromtxt
+import skimage
+from skimage import io
+
+from keras import backend as K
+K.set_learning_phase(1)
+from scipy import io
+
+from keras.applications.vgg19 import VGG19
+
+start_time = time.time()
+
+boxes = genfromtxt('predicted_bounding_boxes.csv', delimiter=',')
+indices= genfromtxt('predicted_image_indices.csv', delimiter=',')
+label= genfromtxt('predicted_image_labels.csv', delimiter=',')
+
+N = len(label)
+# M= np.zeros((N,4096),dtype=float)
+
+M= [ ]
+non_zero_labels= [ ]
+base_model = VGG19(weights='imagenet')
+get_output = K.function([base_model.layers[0].input], [base_model.layers[23].output]) # 23 for VGG19, 20 for VGG16
+base_model.save("vgg19.h5")
+
+for i in range(N):
+
+    I = skimage.io.imread(os.path.join("out_rl", "%i.png" % indices[i]))
+    box = boxes[i,: ].astype(int)
+    J = I[box[1]:box[3], box[0]:box[2]]
+
+    (w,h,d) = J.shape
+    if w == 0 or h == 0:
+        L = np.zeros((224,224,3))
+    else:
+        L = sp.misc.imresize(J,(224,224,3))
+        # print("Positive detection")
+
+        S = L.reshape(1, 224, 224, 3)
+        O = get_output([S])[0]
+        M.append(O.flatten())
+        non_zero_labels.append(label[i])
+
+    Lt = I
+    (w,h,d) = I.shape
+    box[2] = min(box[2], h - 1)
+    box[3] = min(box[3], w - 1)
+    
+    Lt[box[1],box[0]:box[2]] = [0,255,0]
+    Lt[box[3],box[0]:box[2]] = [0,255,0]
+    Lt[box[1]:box[3],box[0]] = [0,255,0]
+    Lt[box[1]:box[3],box[2]] = [0,255,0]
+    print(box)
+    print(w)
+    print(h)
+    plt.imshow(Lt)
+    plt.show()
+
+    # Plug in the trained CNN Model to extract feature
+    # S = L.reshape(1,224,224,3)
+    # O = get_output([S])[0]
+    #
+    # M[i,: ]= O.flatten()
+
+    print(i)
+
+sp.io.savemat("man_feature_1.mat",{"cnn_features":M})
+sp.io.savemat("man_names_1.mat",{"names":label})
+
+print("--- Execution time: %s seconds ---" % (time.time() - start_time))
+
+print("Wait")
+
